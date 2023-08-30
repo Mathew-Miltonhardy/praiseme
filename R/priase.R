6f1c249df0e47e141ac62c0150659543e7dfd136
Mathew Miltonhardy <mathew.miltonhardy@students.mq.edu.au>
2023-08-30 17:56:13 +1000

    Initial commit

---
+++ R/priase.R
@@ -0,0 +1,17 @@
+#' Deliver Praise
+#'
+#' @description
+#' A very nice little function that gives you some praise when you're feeling down.
+#' Please be mindful that overuse can lead to undesirable effects.
+#'
+#' @param name text string; This is the name of the person to praise
+#' @param punctuation text string; This is the emphasis as a text input
+#'
+#' @return Text string with praise
+#' @export
+#'
+#' @examples
+#' praise(name = "Mathew", punctuation = "!")
+praise <- function(name, punctuation = "!"){
+  glue::glue("You're the best, {name}{punctuation}")
+}
